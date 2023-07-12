39cef40d5c0287329e8c68da450e7b7dcd7fa4f8
Isanka Suresh <isankasuresh001@gmail.com>
2023-07-12 14:43:36 +0530

    Add chat window form

---
+++ src/main/java/lk/ijse/liveChatApplication/controller/ChatFormController.java
@@ -0,0 +1,46 @@
+package lk.ijse.liveChatApplication.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ChatFormController {
+
+    @FXML
+    private Label lblName;
+
+    @FXML
+    private TextField txtChat;
+
+    @FXML
+    private JFXButton btnEmoji;
+
+    @FXML
+    private JFXButton btnImage;
+
+    @FXML
+    private JFXButton btnSend;
+
+    @FXML
+    void btnEmojiOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnImageOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSendOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtChatOnAction(ActionEvent event) {
+
+    }
+
+}
