d12edb80f4aa1a9037b2a0ca42feb871cbfc750d
Isanka Suresh <isankasuresh001@gmail.com>
2023-07-13 23:33:24 +0530

    update launcher

--- src/main/java/lk/ijse/liveChatApplication/Launcher.java
+++ src/main/java/lk/ijse/liveChatApplication/Launcher.java
@@ -6,20 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import lk.ijse.liveChatApplication.server.Server;
+
+import java.io.IOException;
 
 public class Launcher extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.initStyle(StageStyle.UNDECORATED);
+        //stage.initStyle(StageStyle.UNDECORATED);
         Parent root = FXMLLoader.load(getClass().getResource("/view/login_form.fxml"));
         stage.setTitle("Login");
         stage.centerOnScreen();
         stage.setScene(new Scene(root));
-
         stage.show();
+
     }
 }
