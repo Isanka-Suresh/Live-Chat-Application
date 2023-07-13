3fb972747e679ddb2905e3f921248c0333346e63
Isanka Suresh <isankasuresh001@gmail.com>
2023-07-13 23:31:24 +0530

    implement the server

---
+++ src/main/java/lk/ijse/liveChatApplication/server/Server.java
@@ -0,0 +1,45 @@
+package lk.ijse.liveChatApplication.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+    private final ServerSocket serverSocket;
+    private static Server server;
+
+    public Server() throws IOException {
+        serverSocket= new ServerSocket(8002);
+        System.out.println("Server Online");
+    }
+
+    public static Server getServer() throws IOException {
+        return server==null ? server= new Server() : server;
+    }
+
+    public void closeServer(){
+        try {
+            if (serverSocket != null){
+                serverSocket.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while(!serverSocket.isClosed()){
+            try {
+                Socket socket= serverSocket.accept();
+                System.out.println("A Client Joined");
+                ClientHandler clientHandler= new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            } catch (IOException e) {
+                closeServer();
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
