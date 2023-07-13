331545adbbfd837e3329b1b7a91bd0a439337257
Isanka Suresh <isankasuresh001@gmail.com>
2023-07-13 23:33:05 +0530

    implement the UI

--- src/main/java/lk/ijse/liveChatApplication/controller/ChatFormController.java
+++ src/main/java/lk/ijse/liveChatApplication/controller/ChatFormController.java
@@ -5,6 +5,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import lk.ijse.liveChatApplication.client.Client;
 
 public class ChatFormController {
 
@@ -15,6 +18,9 @@ public class ChatFormController {
     private TextField txtChat;
 
     @FXML
+    private VBox lblChat;
+
+    @FXML
     private JFXButton btnEmoji;
 
     @FXML
@@ -22,6 +28,8 @@ public class ChatFormController {
 
     @FXML
     private JFXButton btnSend;
+
+    private Client client;
 
     @FXML
     void btnEmojiOnAction(ActionEvent event) {
@@ -35,7 +43,29 @@ public class ChatFormController {
 
     @FXML
     void btnSendOnAction(ActionEvent event) {
-
+        HBox hBox = new HBox();
+        hBox.setStyle(
+                "-fx-alignment: center-right;" +
+                        " -fx-fill-height: true;" +
+                        " -fx-min-height: 50px;" +
+                        " -fx-pref-width: 575px;" +
+                        " -fx-max-width: 575px;" +
+                        " -fx-padding: 10px"
+        );
+        Label label = new Label(txtChat.getText());
+        label.setStyle(
+                " -fx-alignment: center-right;" +
+                        " -fx-background-color:  #3a86ff;" +
+                        " -fx-background-radius:15px;" +
+                        " -fx-font-size: 18px;" +
+                        " -fx-text-fill: #ffffff;" +
+                        " -fx-wrap-text: true;" +
+                        " -fx-content-display: left;" +
+                        " -fx-max-width: 350px;" +
+                        " -fx-padding: 10px;"
+        );
+        hBox.getChildren().add(label);
+        lblChat.getChildren().add(hBox);
     }
 
     @FXML
@@ -43,4 +73,34 @@ public class ChatFormController {
 
     }
 
+    public void setClient(Client client) {
+        this.client=client;
+        lblName.setText(client.getUserName()+"'s Chat");
+    }
+
+    public void displayMsg(String message) {
+        HBox hBox = new HBox();
+        hBox.setStyle(
+                "-fx-alignment: center-left;" +
+                        " -fx-fill-height: true;" +
+                        " -fx-min-height: 50px;" +
+                        " -fx-pref-width: 575px;" +
+                        " -fx-max-width: 575px;" +
+                        " -fx-padding: 10px"
+        );
+        Label label = new Label(message);
+        label.setStyle(
+                " -fx-alignment: center-left;" +
+                        " -fx-background-color:  #cfdee7;" +
+                        " -fx-background-radius:15px;" +
+                        " -fx-font-size: 18px;" +
+                        " -fx-text-fill: #000000;" +
+                        " -fx-wrap-text: true;" +
+                        " -fx-content-display: left;" +
+                        " -fx-max-width: 350px;" +
+                        " -fx-padding: 10px;"
+        );
+        hBox.getChildren().add(label);
+        lblChat.getChildren().add(hBox);
+    }
 }
--- src/main/java/lk/ijse/liveChatApplication/controller/LoginFormController.java
+++ src/main/java/lk/ijse/liveChatApplication/controller/LoginFormController.java
@@ -3,24 +3,50 @@ package lk.ijse.liveChatApplication.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import lk.ijse.liveChatApplication.client.Client;
+import lk.ijse.liveChatApplication.server.Server;
+
+import java.io.IOException;
 
 public class LoginFormController {
+
+    @FXML
+    private AnchorPane anchorLogin;
 
     @FXML
     private TextField txtname;
 
     @FXML
-    private JFXButton btnStart;
+    private Label lblError;
 
     @FXML
-    void btnStartOnAction(ActionEvent event) {
+    private JFXButton btnStart;
 
+
+    @FXML
+    void btnStartOnAction(ActionEvent event) throws IOException {
+        new Client(txtname.getText());
+        txtname.clear();
+        serverOnline();
     }
 
     @FXML
     void txtnameOnAction(ActionEvent event) {
 
     }
+
+    private void serverOnline() throws IOException {
+        Server server = Server.getServer();
+        Thread thread = new Thread(server);
+        thread.start();
+    }
+
 
 }
